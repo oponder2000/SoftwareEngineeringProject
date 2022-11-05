53aa670b9b74313174adba9f9468ccf70eb96883
Nicolas-hr00 <80604439+Nicolas-hr00@users.noreply.github.com>
2022-11-05 10:38:07 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter , Route, Routes, Link} from "react-router-dom";
 import './App.css';
+import Home from './Components/Pages/Home';
+import React  from "react";
+import Navbar from './Components/Navbar';
+import { Button } from "./Components/Button";
 
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  return(
+    <>
+    <BrowserRouter>
+       <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/> 
+        </Routes>
+    </BrowserRouter>    
+      </>
+  );  
+}
 export default App;
