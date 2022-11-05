b79ed48fb5430eddedb058266c9f60404199b9bd
Nicolas-hr00 <80604439+Nicolas-hr00@users.noreply.github.com>
2022-11-05 10:45:54 -0500

    Create Home.js

---
+++ src/Components/Pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../App.css';
+import Footer from '../Footer';
+import Menu from '../Menu';
+
+function Home () {
+    return (
+        <>  
+            <Menu /*thsi calls the menu function - video & text*//>
+            <Footer/>  
+        </>
+
+    );
+}
+
+export default Home;
