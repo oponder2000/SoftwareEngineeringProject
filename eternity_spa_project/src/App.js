0b860f8eeccaba4c1c4ffdb0a77cd81eb7144751
oponder2000 <oponder2000@gmail.com>
2022-11-08 15:03:13 -0600

    added changes to App.js, called treatment page

--- eternity_spa_project/src/App.js
+++ eternity_spa_project/src/App.js
@@ -4,6 +4,7 @@ import Home from './Components/Pages/Home';
 import React  from "react";
 import Navbar from './Components/Navbar';
 import { Button } from "./Components/Button";
+import Treatment from './Components/Pages/Treatment';
 
  
 function App() {
@@ -14,6 +15,7 @@ function App() {
        <Navbar/>
         <Routes>
           <Route path="/" element={<Home/>}/> 
+          <Route path="/Treatments" element={<Treatment/>}/>
         </Routes>
     </BrowserRouter>    
       </>
