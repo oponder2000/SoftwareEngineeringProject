38e7d9ebc9e452f6413a485df2668ba1cdc68521
Geary-E <geary16.erua@gmail.com>
2022-11-13 23:30:34 -0600

    Made image size smaller

--- eternity_spa_project/src/Components/Pages/Employee.js
+++ eternity_spa_project/src/Components/Pages/Employee.js
@@ -1,71 +1,77 @@
-
 import React from 'react';
 import '../Employee.css';
-//import doctor1 from './doctor7.jpg';
+
 
 function Employee(props) {
     return (
         <div className="Employee">
             <div className="spaces">
+
                 <div className="grid1">
-            <img className="img-1" alt="doctor" src={props.src} />
-            <div className="bio-box">
-            <h1><u> Bio</u></h1>
+                    
+                    <img className="img-1" alt="doctor_1" src={props.src} />
+
+                    <div className="bio-box">
+                        <h2><u> Bio</u></h2>
                             <p className="p3">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
                              ut labore et dolore magna aliqua. Molestie a iaculis at erat pellentesque. 
                              Tempus egestas sed sed risus pretium quam vulputate dignissim. 
                              Platea dictumst vestibulum rhoncus est pellentesque elit. 
                              Enim ut sem viverra aliquet eget sit amet tellus. 
-                             Sit amet nulla facilisi morbi tempus. Fames ac turpis egestas maecenas. 
-                             Tristique senectus et netus et malesuada fames ac turpis. 
-                             Sed viverra ipsum nunc aliquet bibendum enim facilisis. 
-                             Ut porttitor leo a diam sollicitudin.
-                             Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras.
+                             Sit amet nulla facilisi morbi tempus. 
                             </p>
-                            </div>
-            </div> 
-            <div className="grid2"> 
-            <h1><u> James Thomas </u></h1><br />
-            <div className="about-box">
-                <h1><u> About Work </u></h1> <br />
-                <p className="p2"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                 Nunc mi ipsum faucibus vitae aliquet nec. Gravida in fermentum et sollicitudin. 
-                 Diam in arcu cursus euismod quis viverra nibh cras pulvinar. 
-                 At urna condimentum mattis pellentesque id nibh tortor id. 
-                 Lectus nulla at volutpat diam ut. Ornare arcu odio ut sem nulla. 
-                 Sapien pellentesque habitant morbi tristique senectus. 
-                 Odio aenean sed adipiscing diam donec adipiscing tristique. 
-                 Netus et malesuada fames ac. Blandit turpis cursus in hac habitasse. 
-                 Et leo duis ut diam quam nulla. Ipsum a arcu cursus vitae congue mauris rhoncus aenean. 
-                 Id aliquet lectus proin nibh nisl condimentum id venenatis a. Ut eu sem integer vitae justo. 
-                 Eu lobortis elementum nibh tellus molestie nunc non blandit massa. 
-                 Enim sed faucibus turpis in eu mi bibendum neque. 
-                 Tristique nulla aliquet enim tortor at auctor urna. 
-                 Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit lectus. 
-                 Tincidunt praesent semper feugiat nibh sed pulvinar proin.</p><br />
+                            </div><br /><br /> {/* Bio - box */}
 
-                 <p className="p1"> Curabitur gravida arcu ac tortor dignissim. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. 
-                    Ipsum nunc aliquet bibendum enim facilisis gravida. Eget mi proin sed libero. 
-                    Pharetra pharetra massa massa ultricies mi quis. Fusce id velit ut tortor pretium viverra suspendisse. 
-                    Nam at lectus urna duis convallis convallis tellus id. Vestibulum morbi blandit cursus risus.
-                     Elementum sagittis vitae et leo duis. Nec ultrices dui sapien eget mi proin. 
-                     Pharetra magna ac placerat vestibulum.</p> <br /></div>
-            <br />
+                            <div className="education">
+                                <h2><u> Education</u></h2>
+                                <p className="p4">
+                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                 Tempus imperdiet nulla malesuada pellentesque elit. 
+                                 Velit egestas dui id ornare arcu odio.
+                                </p>
+                            </div>  {/* Education box */}
+
+                        </div> {/* End of Grid 1 */}
+
+                <div className="grid2"> 
+                    <h2><u> James Thomas </u></h2><br />
+                    <div className="about-box">
+                    <h2><u> About Work </u></h2> <br />
+                    <p className="p1"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Nunc mi ipsum faucibus vitae aliquet nec. Gravida in fermentum et sollicitudin. 
+                        Diam in arcu cursus euismod quis viverra nibh cras pulvinar. 
+                        At urna condimentum mattis pellentesque id nibh tortor id. 
+                        Lectus nulla at volutpat diam ut. Ornare arcu odio ut sem nulla. 
+                        Sapien pellentesque habitant morbi tristique senectus. 
+                        Odio aenean sed adipiscing diam donec adipiscing tristique. 
+                        Netus et malesuada fames ac. Blandit turpis cursus in hac habitasse. 
+                        Et leo duis ut diam quam nulla. Ipsum a arcu cursus vitae congue mauris rhoncus aenean. 
+                        Id aliquet lectus proin nibh nisl condimentum id venenatis a. Ut eu sem integer vitae justo. 
+                        Eu lobortis elementum nibh tellus molestie nunc non blandit massa. 
+                        Enim sed faucibus turpis in eu mi bibendum neque. 
+                        Tristique nulla aliquet enim tortor at auctor urna. 
+                        Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit lectus. 
+                        Tincidunt praesent semper feugiat nibh sed pulvinar proin.</p>
+                     </div> <br /> {/* End of about box */}
+
             <div className="work-box">
-                <h1><u> Work Experience </u></h1> 
-                <ul className="l1">
-                    <li> Job 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            <h2><u> Work Experience </u></h2> 
+            <ul>
+                <li> Job 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-                    <li> Job 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                <li> Job 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-                    <li> Job 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                <li> Job 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>         
                 </ul>
-                </div>
-                </div>
-                </div>    
-        </div>
+                </div> {/* End of work-box */}
+
+                </div> {/* End of grid 2 */}
+
+                </div>    {/* End of spaces */}
+        </div>          /* End of Employee */
     )
 }
 
